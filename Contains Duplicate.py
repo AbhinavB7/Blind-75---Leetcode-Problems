77c575dab6017f3a63fc8ffa25718f7e22eee002
Abhinav Bhamidipati <87815926+AbhinavB7@users.noreply.github.com>
2024-02-02 01:59:16 -0500

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,3 +1,4 @@
+## 1 - Compare adjacent numbers after sorting
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort() #sort list in ascending order 
@@ -5,4 +6,15 @@ class Solution:
         for i in range(1,n):
             if nums[i]==nums[i-1]: #compare adjacent elements 
                 return True
+        return False
+
+
+## 2 - Compare every element with each other
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(0,n-1):
+            for j in range(i+1, n):
+                if nums[i]==nums[j]: #compare every element
+                    return True
         return False
