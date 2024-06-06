31ab2c5d3d526ad4f8ccdc4b58ceb5a78d322f27
AbhinavB7 <babhinavmohan@gmail.com>
2024-06-06 15:28:27 -0400

    new

--- Counting Bits.py
+++ Counting Bits.py
@@ -1,14 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp = [0]
+        
+        ans = []
 
-        for i in range(1, n + 1):
-            # If the current number is odd (last bit is 1), add 1 to the count.
-            if i % 2 == 1:
-                dp.append(dp[i - 1] + 1)
-            else:
-                # If the current number is even, count of set bits is the same as for i/2.
-                dp.append(dp[i // 2])
-
-        # Return the final list
-        return dp
+        for a in range(0,n+1):
+            count = 0
+            a = bin(a)
+            for i in a:
+                if i == '1':
+                    count += 1
+            ans.append(count)
+        
+        return ans
